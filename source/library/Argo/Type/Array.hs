b9ba60a17fc75daee42fcfad6faefa885d13f19a
Taylor Fausak <taylor@fausak.me>
2021-10-09 22:10:33 +0000

    Add `NFData` instances

--- source/library/Argo/Type/Array.hs
+++ source/library/Argo/Type/Array.hs
@@ -2,8 +2,12 @@ module Argo.Type.Array
     ( Array(..)
     ) where
 
+import qualified Control.DeepSeq as DeepSeq
 import qualified Data.Array as Array
 
 newtype Array a
     = Array (Array.Array Int a)
     deriving (Eq, Show)
+
+instance DeepSeq.NFData a => DeepSeq.NFData (Array a) where
+    rnf (Array x) = DeepSeq.rnf x
--- source/library/Argo/Type/Null.hs
+++ source/library/Argo/Type/Null.hs
@@ -2,6 +2,11 @@ module Argo.Type.Null
     ( Null(..)
     ) where
 
+import qualified Control.DeepSeq as DeepSeq
+
 newtype Null
     = Null ()
     deriving (Eq, Show)
+
+instance DeepSeq.NFData Null where
+    rnf (Null x) = DeepSeq.rnf x
