48e0a280d6fbbc2b9d7f279e705d994673033121
Taylor Fausak <taylor@fausak.me>
2021-10-10 15:14:17 +0000

    Implement executable

--- source/executable/Main.hs
+++ source/executable/Main.hs
@@ -1,4 +1,11 @@
-import Argo ()
+import qualified Argo
+import qualified Data.ByteString as ByteString
+import qualified Data.ByteString.Builder as Builder
+import qualified System.IO as IO
 
 main :: IO ()
-main = interact id
+main = do
+    contents <- ByteString.getContents
+    case Argo.decode contents of
+        Nothing -> fail "invalid JSON"
+        Just value -> Builder.hPutBuilder IO.stdout $ Argo.encode value
