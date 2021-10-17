fac5a8689df544310902f386b4899d2e88e9fffd
Taylor Fausak <taylor@fausak.me>
2021-10-17 13:09:32 +0000

    Remove overlapping instance

--- source/library/Argo/Class/ToValue.hs
+++ source/library/Argo/Class/ToValue.hs
@@ -5,8 +5,6 @@ module Argo.Class.ToValue where
 import qualified Argo.Type.Array as Array
 import qualified Argo.Type.Boolean as Boolean
 import qualified Argo.Type.Number as Number
-import qualified Argo.Type.Object as Object
-import qualified Argo.Type.Pair as Pair
 import qualified Argo.Type.String as String
 import qualified Argo.Type.Value as Value
 import qualified Data.Array
@@ -29,6 +27,3 @@ instance ToValue Text.Text where
 
 instance ToValue a => ToValue (Data.Array.Array Int a) where
     toValue = Value.Array . Array.Array . fmap toValue
-
-instance ToValue a => ToValue (Data.Array.Array Int (Pair.Pair String.String a)) where
-    toValue = Value.Object . Object.Object . fmap (\ (Pair.Pair (k, v)) -> Pair.Pair (k, toValue v))
