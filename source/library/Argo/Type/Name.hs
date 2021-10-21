832083f12fb8f7630bd4c39382a7979ddfedaadd
Taylor Fausak <taylor@fausak.me>
2021-10-21 23:04:28 +0000

    Introduce "name" type

---
+++ source/library/Argo/Type/Name.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE TemplateHaskellQuotes #-}
+
+module Argo.Type.Name where
+
+import qualified Argo.Decoder as Decoder
+import qualified Argo.Type.String as String
+import qualified Control.DeepSeq as DeepSeq
+import qualified Data.ByteString.Builder as Builder
+import qualified Language.Haskell.TH.Syntax as TH
+
+newtype Name
+    = Name String.String
+    deriving (Eq, Show)
+
+instance TH.Lift Name where
+    liftTyped (Name x) = [|| Name x ||]
+
+instance DeepSeq.NFData Name where
+    rnf (Name x) = DeepSeq.rnf x
+
+encode :: Name -> Builder.Builder
+encode (Name x) = String.encode x
+
+decode :: Decoder.Decoder Name
+decode = Name <$> String.decode
