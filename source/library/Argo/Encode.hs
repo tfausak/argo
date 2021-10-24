d8fa2ee9476e0cad7dd30a8e488a4edcb7848d1b
Taylor Fausak <taylor@fausak.me>
2021-10-24 21:31:35 +0000

    Expose and test pretty printing

--- source/library/Argo/Encode.hs
+++ source/library/Argo/Encode.hs
@@ -3,19 +3,24 @@ module Argo.Encode where
 import qualified Argo.Class.ToValue as ToValue
 import qualified Argo.Encoder as Encoder
 import qualified Argo.Json.Value as Value
+import qualified Argo.Literal as Literal
 import qualified Argo.Vendor.Builder as Builder
 import qualified Argo.Vendor.Transformers as Trans
+import qualified Control.Monad as Monad
 import qualified Data.Functor.Identity as Identity
 
 encode :: ToValue.ToValue a => a -> Builder.Builder
-encode = encodeWith Encoder.Config
-    { Encoder.indent = Encoder.Spaces 0
-    , Encoder.level = 0
-    }
+encode = encodeWith $ Encoder.Spaces 0
 
-encodeWith :: ToValue.ToValue a => Encoder.Config -> a -> Builder.Builder
-encodeWith c x = Identity.runIdentity
+encodeWith :: ToValue.ToValue a => Encoder.Indent -> a -> Builder.Builder
+encodeWith i x =
+    let c = Encoder.Config { Encoder.indent = i, Encoder.level = 0 }
+    in Identity.runIdentity
     . Trans.execWriterT
     . flip Trans.runReaderT c
     $ do
         Value.encode $ ToValue.toValue x
+        Monad.when (Encoder.hasIndent c)
+            . Trans.lift
+            . Trans.tell
+            $ Builder.word8 Literal.newLine
