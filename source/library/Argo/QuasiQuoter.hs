24d345344e6d8f32951acfacd08ac59967004669
Taylor Fausak <taylor@fausak.me>
2021-11-07 15:06:21 +0000

    Inline `quoteExp`

--- source/library/Argo/QuasiQuoter.hs
+++ source/library/Argo/QuasiQuoter.hs
@@ -9,12 +9,9 @@ import qualified Argo.Vendor.Text as Text
 value :: TH.QuasiQuoter
 value = TH.QuasiQuoter
     { TH.quoteDec = const $ fail "cannot be used as a declaration"
-    , TH.quoteExp = quoteExp
+    , TH.quoteExp = \ x -> case Decode.decode . Text.encodeUtf8 $ Text.pack x of
+        Result.Failure e -> fail e
+        Result.Success y -> TH.lift (y :: Value.Value)
     , TH.quotePat = const $ fail "cannot be used as a pattern"
     , TH.quoteType = const $ fail "cannot be used as a type"
     }
-
-quoteExp :: String -> TH.Q TH.Exp
-quoteExp x = case Decode.decode . Text.encodeUtf8 $ Text.pack x of
-    Result.Failure e -> fail e
-    Result.Success y -> TH.lift (y :: Value.Value)
