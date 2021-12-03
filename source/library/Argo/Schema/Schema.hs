6bb5f5a13e0aa431b7a0be7c31e2f5d73d04ca01
Taylor Fausak <taylor@fausak.me>
2021-12-03 01:45:18 +0000

    Add semigroup and monoid instances to schema

--- source/library/Argo/Schema/Schema.hs
+++ source/library/Argo/Schema/Schema.hs
@@ -4,9 +4,15 @@
 
 module Argo.Schema.Schema where
 
+import qualified Argo.Json.Array as Array
+import qualified Argo.Json.Member as Member
+import qualified Argo.Json.Name as Name
+import qualified Argo.Json.Object as Object
+import qualified Argo.Json.String as String
 import qualified Argo.Json.Value as Value
 import qualified Argo.Vendor.DeepSeq as DeepSeq
 import qualified Argo.Vendor.TemplateHaskell as TH
+import qualified Data.Text as Text
 import qualified GHC.Generics as Generics
 
 -- | A JSON Schema.
@@ -14,6 +20,17 @@ import qualified GHC.Generics as Generics
 newtype Schema
     = Schema Value.Value
     deriving (Eq, Generics.Generic, TH.Lift, DeepSeq.NFData, Show)
+
+instance Semigroup Schema where
+    x <> y = fromValue . Value.Object $ Object.fromList
+        [ Member.fromTuple
+            ( Name.fromString . String.fromText $ Text.pack "oneOf"
+            , Value.Array $ Array.fromList [toValue x, toValue y]
+            )
+        ]
+
+instance Monoid Schema where
+    mempty = fromValue . Value.Object $ Object.fromList []
 
 fromValue :: Value.Value -> Schema
 fromValue = Schema
