ffe87b3ef539bf0ac9433555e1cf0700478cab28
Taylor Fausak <taylor@fausak.me>
2022-01-23 16:02:06 +0000

    Remove "-T" suffix

--- source/library/Argo/Vendor/Transformers.hs
+++ source/library/Argo/Vendor/Transformers.hs
@@ -1,31 +1,31 @@
 module Argo.Vendor.Transformers
-    ( AccumT.AccumT
-    , ExceptT.ExceptT
-    , MaybeT.MaybeT
-    , ReaderT.ReaderT
-    , StateT.StateT
-    , WriterT.WriterT
-    , AccumT.add
-    , ReaderT.ask
-    , StateT.get
+    ( Accum.AccumT
+    , Except.ExceptT
+    , Maybe.MaybeT
+    , Reader.ReaderT
+    , State.StateT
+    , Writer.WriterT
+    , Accum.add
+    , Reader.ask
+    , State.get
     , Trans.lift
-    , ReaderT.local
-    , AccumT.look
-    , StateT.put
-    , AccumT.runAccumT
-    , ExceptT.runExceptT
-    , MaybeT.runMaybeT
-    , ReaderT.runReaderT
-    , StateT.runStateT
-    , WriterT.runWriterT
-    , WriterT.tell
-    , ExceptT.throwE
+    , Reader.local
+    , Accum.look
+    , State.put
+    , Accum.runAccumT
+    , Except.runExceptT
+    , Maybe.runMaybeT
+    , Reader.runReaderT
+    , State.runStateT
+    , Writer.runWriterT
+    , Writer.tell
+    , Except.throwE
     ) where
 
-import qualified Control.Monad.Trans.Accum as AccumT
+import qualified Control.Monad.Trans.Accum as Accum
 import qualified Control.Monad.Trans.Class as Trans
-import qualified Control.Monad.Trans.Except as ExceptT
-import qualified Control.Monad.Trans.Maybe as MaybeT
-import qualified Control.Monad.Trans.Reader as ReaderT
-import qualified Control.Monad.Trans.State as StateT
-import qualified Control.Monad.Trans.Writer as WriterT
+import qualified Control.Monad.Trans.Except as Except
+import qualified Control.Monad.Trans.Maybe as Maybe
+import qualified Control.Monad.Trans.Reader as Reader
+import qualified Control.Monad.Trans.State as State
+import qualified Control.Monad.Trans.Writer as Writer
